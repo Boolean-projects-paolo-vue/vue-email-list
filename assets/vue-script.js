893230ad53608571b3360044637f70482c2c2afe
Paolo96700 <paolo.falcoapp@gmail.com>
2023-05-17 12:34:30 +0200

    aggiunta

--- assets/vue-script.js
+++ assets/vue-script.js
@@ -1,13 +1,14 @@
 const app = Vue.createApp({
     data() {
         return {
-            mailArray:[]
+            mailArray:[],
+            numberMails: 10,
         };
     },
     methods: {
         generateRandomMail() {
             this.mailArray = [];
-            for (let i = 0; i < 10; i++) {
+            for (let i = 0; i < this.numberMails; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(serverResponse => this.mailArray.push(serverResponse.data.response));
             }
         },
